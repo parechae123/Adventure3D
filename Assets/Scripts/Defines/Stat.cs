a23bfdba192b7357b01535ece28b78e4cb7f70de
Heoyoon <parechae@gmail.com>
2025-08-09 01:52:54 +0900

    feat : 버프 객체 추가 및 지속시간 계산

--- Assets/Scripts/Defines/Stat.cs
+++ Assets/Scripts/Defines/Stat.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Unity.VisualScripting;
+using UnityEditor;
 
 namespace Defines
 {
@@ -19,30 +20,64 @@ namespace Defines
             } 
             set 
             {
-                if (MaxHP < value) return;
-                
+                hpBar.Invoke(((float)value / (float)maxHP));
+                if (MaxHP < value)
+                {
+                    return;
+                }
+                if(value <= 0) { Player.input.DisablePlayer(); currHP = 0; }
                 currHP = value;
             } 
         }
         public int MaxHP { get { return maxHP; } set { maxHP = value; } }
 
         private float moveSpeed;
-        public float MoveSpeed { get { return moveSpeed; } set { moveSpeed = value; } }
+        private float buffMoveSpeed;
+        public float MoveSpeed { get { return buffMoveSpeed+moveSpeed; } set { moveSpeed = value; } }
+        public float BuffMoveSpeed {set { buffMoveSpeed = value; } }
 
         private float jumpForce;
         public float JumpForce { get { return jumpForce; } set { jumpForce = value; } }
         public bool IsDie { get { return currHP <= 0; } }
-
+        public Action<float> hpBar;
+        public List<Buff> moveBuffs = new List<Buff>();
         public Stat(int hp, float moveSpeed, float jumpForce)
         {
             this.maxHP = hp;
             this.currHP = hp;
             this.moveSpeed = moveSpeed;
             this.jumpForce = jumpForce;
+            GameManager.GetInstance.moveBuff += GetMoveSpeedBuff;
         }
         public void GetDamaged(int damage)
         {
             HP -= damage;
+        }
+        public void GetHeal(int val)
+        {
+            HP += val;
+        }
+        public void GetMoveSpeedBuff(int value,float duration) 
+        {
+            buffMoveSpeed += value;
+            
+            moveBuffs.Add(new Buff(value, duration, ReleaseMoveSpeedBuff));
+        }
+        public void ReleaseMoveSpeedBuff(int value) 
+        {
+            buffMoveSpeed -= value;
+        }
+    }
+    public class Buff
+    {
+        public int itemValue;
+        public float duration;
+        public Action<int> action;
+        public Buff(int itemValue, float duration, Action<int> action)
+        {
+            this.itemValue = itemValue;
+            this.duration = duration;
+            this.action = action;
         }
     }
 }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -18,9 +18,11 @@ public class Player : MonoBehaviour
         stat = new Stat(100, 5f, 6f);
         moveHandler = new PlayerMoveHandler(rb,stat,transform);
         cam = new PlayerCam(transform);
-        if(input==null)input = new InputHandler(moveHandler.OnMove, moveHandler.OnJump, cam.RotCamera);
+        if(input==null)input = new InputHandler(moveHandler.OnMove, moveHandler.OnJump, cam.RotCamera,cam.InfoRay);
         else { Destroy(gameObject);Debug.LogError("중복된 플레이어 객체가 있습니다"); }
         Cursor.visible = false;
+        stat.hpBar += GameObject.FindGameObjectWithTag("Canvas").transform.Find("HPBar").GetComponent<HPBar>().OnValueChange;
+        GameManager.GetInstance.damageDict.Add(GetComponent<Collider>(), stat.GetDamaged);
     }
     void Start()
     {
@@ -29,6 +31,20 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (stat.moveBuffs != null&& stat.moveBuffs.Count>0)
+        {
+            for (int i = 0; i < stat.moveBuffs.Count; i++)
+            {
+                if (i < 0) break;
+                stat.moveBuffs[i].duration -= Time.deltaTime;
+                if (stat.moveBuffs[i].duration <= 0f)
+                {
+                    stat.moveBuffs[i].action.Invoke(stat.moveBuffs[i].itemValue);
+                    stat.moveBuffs.RemoveAt(i);
+                    i -= 1;
+                }
+            }
+        }
         input.InputUpdate();
 /*        if (input.MovePress())
         {
@@ -37,8 +53,12 @@ public class Player : MonoBehaviour
     }
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.layer == 3) moveHandler.IsGround();
+        if (collision.gameObject.layer == 3) moveHandler.IsGround(true);
 
         if (collision.gameObject.layer == 6) GameManager.GetInstance.interactionDict[collision.collider].Invoke(rb);
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.layer == 3) moveHandler.IsGround(false);
     }
 }
