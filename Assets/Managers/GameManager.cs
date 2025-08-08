371a2ab4e72c36e2064155047df64b2efa64be3e
Heoyoon <parechae@gmail.com>
2025-08-09 01:52:07 +0900

    feat : 정보조사,버프,피해 액션 추가

--- Assets/Managers/GameManager.cs
+++ Assets/Managers/GameManager.cs
@@ -6,6 +6,10 @@ using UnityEngine;
 public class GameManager : SingleTon<GameManager>
 {
     public Dictionary<Collider, Action<Rigidbody>> interactionDict = new Dictionary<Collider, Action<Rigidbody>>();
+    public Dictionary<Collider, Action> infoData = new Dictionary<Collider, Action>();
+    public Dictionary<Collider, Action<int>> damageDict = new Dictionary<Collider, Action<int>>();
+    public Action<int, float> moveBuff;
+    public Action<string> printInfo;
     protected override void Init()
     {
 
